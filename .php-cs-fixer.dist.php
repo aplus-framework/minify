74eeeae331bd65bfbda18c00e16ab383364ba414
Natan Felles <natanfelles@gmail.com>
2021-07-11 15:12:29 -0300

    Indent files with space

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -4,5 +4,5 @@ use Framework\CodingStandard\Config;
 use Framework\CodingStandard\Finder;
 
 return Config::create()->setFinder(
-	Finder::create()->in(__DIR__)
+    Finder::create()->in(__DIR__)
 );
