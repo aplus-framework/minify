16dcf560ee50d035113a3f4e53037719d8c534c5
Natan Felles <natanfelles@gmail.com>
2022-06-23 19:53:01 -0300

    Update Coding Standard config file

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,8 +1,18 @@
-<?php
-
+<?php declare(strict_types=1);
+/*
+ * This file is part of Aplus Framework Minify Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 use Framework\CodingStandard\Config;
 use Framework\CodingStandard\Finder;
 
-return Config::create()->setFinder(
+return (new Config())->setDefaultHeaderComment(
+    'Aplus Framework Minify Library',
+    'Natan Felles <natanfelles@gmail.com>'
+)->setFinder(
     Finder::create()->in(__DIR__)
 );
